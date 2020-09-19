9aa1f67ae155f974a01c2d465f1c55ce0f1bbf37
theacheng <theacheng.g6@gmail.com>
2020-09-19 17:15:14 +0700

    done rotate animation

--- lib/animations/rotate_animation.dart
+++ lib/animations/rotate_animation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'package:flutter_animation/constant/curves.dart';
+
 class RotateAnimation extends StatefulWidget {
   const RotateAnimation({Key key}) : super(key: key);
 
@@ -12,6 +14,8 @@ class _RotateAnimationState extends State<RotateAnimation>
     with SingleTickerProviderStateMixin {
   AnimationController animationController;
   Animation<double> animation;
+  Curve _currentCurve;
+  Curve _reverseCurve;
 
   @override
   void initState() {
@@ -21,10 +25,13 @@ class _RotateAnimationState extends State<RotateAnimation>
       vsync: this,
     );
 
-    final _curvedAnimation = CurvedAnimation(
+    _currentCurve = Curves.bounceIn;
+    _reverseCurve = Curves.bounceOut;
+
+    CurvedAnimation _curvedAnimation = CurvedAnimation(
       parent: animationController,
-      curve: Curves.bounceOut,
-      reverseCurve: Curves.bounceIn,
+      curve: _currentCurve,
+      reverseCurve: _reverseCurve,
     );
 
     animation = Tween<double>(
@@ -35,37 +42,114 @@ class _RotateAnimationState extends State<RotateAnimation>
           ..addListener(() => setState(() {}))
           ..addStatusListener(
             (status) {
+              _curvedAnimation.reverseCurve = _reverseCurve;
+              _curvedAnimation.curve = _currentCurve;
               if (status == AnimationStatus.completed) {
+                print(_curvedAnimation.reverseCurve);
                 animationController.reverse();
               } else if (status == AnimationStatus.dismissed) {
+                print(_curvedAnimation.curve);
                 animationController.forward();
               }
             },
           );
-
     animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
+    List<DropdownMenuItem> curvesList = [];
+    for (int i = 0; i < curves.curves.length; i++) {
+      curvesList.add(
+        DropdownMenuItem(
+          child: Row(
+            children: [
+              Icon(Icons.arrow_drop_down),
+              Text('${curves.curveName[i]}'),
+            ],
+          ),
+          value: curves.curves[i],
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Rotate Animation'),
       ),
-      body: Center(
-        child: Transform.rotate(
-          angle: animation.value,
-          child: FlutterLogo(
-            size: MediaQuery.of(context).size.width * .3,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Forward Curve",
+                style: TextStyle(fontSize: 18),
+              ),
+              DropdownButton(
+                items: curvesList,
+                value: _currentCurve,
+                onChanged: (value) {
+                  setState(() {
+                    _currentCurve = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Reverse Curve",
+                style: TextStyle(fontSize: 18),
+              ),
+              DropdownButton(
+                items: curvesList,
+                value: _reverseCurve,
+                onChanged: (value) {
+                  setState(() {
+                    _reverseCurve = value;
+                  });
+                },
+              ),
+            ],
           ),
-        ),
+          Center(
+            child: Transform.rotate(
+              angle: animation.value,
+              child: FlutterLogo(
+                size: MediaQuery.of(context).size.width * .3,
+              ),
+            ),
+          ),
+          SizedBox(),
+          FlatButton(
+            color: Colors.blue.shade500,
+            child: Text(
+              !animationController.isAnimating ? "Animate" : "Stop",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            onPressed: () {
+              setState(() {
+                if (animationController.isAnimating)
+                  animationController.stop();
+                else {
+                  if (animationController.status == AnimationStatus.reverse)
+                    animationController.reverse();
+                  else if (animationController.status ==
+                      AnimationStatus.forward) animationController.forward();
+                }
+              });
+            },
+          ),
+          SizedBox(),
+        ],
       ),
     );
   }
 
   @override
   void dispose() {
-    super.dispose();
+    //will be error if call dispose after super.dispose()
     animationController.dispose();
+    super.dispose();
   }
 }
